96570a133af1bf6d8d36972c3f2e970142127f01
Vishwak Koleti <koletivishwak84@gmail.com>
2026-06-13 21:14:16 +0530

    Mobile UI Updated [ NavBar ]

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
                 <img className='w-8 rounded-full' src={userData.image} alt="profile_pic" />
                 <img className='w-2.5' src={assets.dropdown_icon} alt='dropdown_icon' />
 
-                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20'>
+                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden'>
                     <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                         <p onClick={()=> navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                         <p onClick={()=> navigate("/my-appointments")} className='hover:text-black cursor-pointer'>My Appointments</p>
