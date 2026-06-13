bbf5e0a4e41f7715194f4e190b7de5bab1f2898a
Vishwak Koleti <koletivishwak84@gmail.com>
2026-06-13 21:24:59 +0530

    UI Changes Done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,92 +1,175 @@
-import { NavLink, useNavigate } from 'react-router-dom'
-import {assets} from '../assets/assets'
-import { useContext, useState } from 'react'
+import { NavLink, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { AppContext } from '../context/AppContext';
+import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const { token, setToken, userData } = useContext(AppContext);
 
-    const { token, setToken, userData } = useContext(AppContext)
+  const [showMenu, setShowMenu] = useState(false);
 
-    const [showMenu, setShowMenu] = useState(false);
-
-    const logout = () => {
-        setToken(false)
-        localStorage.removeItem('token')
-    }
+  const logout = () => {
+    setToken(false);
+    localStorage.removeItem("token");
+  };
 
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <Link to="/">
-        <img 
-            className='w-44 cursor-pointer' 
-            src={assets.logo} 
-            alt="logo" 
-        />
+        <img className="w-44 cursor-pointer" src={assets.logo} alt="logo" />
       </Link>
-      <ul className='hidden md:flex items-start gap-5 font-medium'>
-        <NavLink to='/'>
-            <li className='py-1'>HOME</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+      <ul className="hidden md:flex items-start gap-5 font-medium">
+        <NavLink to="/">
+          <li className="py-1">HOME</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to='/doctors'>
-            <li className='py-1'>ALL DOCTORS</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+        <NavLink to="/doctors">
+          <li className="py-1">ALL DOCTORS</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to='/about'>
-            <li className='py-1'>ABOUT</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+        <NavLink to="/about">
+          <li className="py-1">ABOUT</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to='/contact'>
-            <li className='py-1'>CONTACT</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+        <NavLink to="/contact">
+          <li className="py-1">CONTACT</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        <NavLink to='https://prescripto-rosy-theta.vercel.app'>
-            <li className='py-1 font-extrabold'>ADMIN PANEL</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+        <NavLink to="https://prescripto-rosy-theta.vercel.app">
+          <li className="py-1 font-extrabold">ADMIN PANEL</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
-        
       </ul>
-      <div className='flex items-center gap-4'>
-        {
-            token && userData
-            ? <div className='flex items-center gap-2 cursor-pointer relative'>
-                <img className='w-8 rounded-full' src={userData.image} alt="profile_pic" />
-                <img className='w-2.5' src={assets.dropdown_icon} alt='dropdown_icon' />
+      <div className="flex items-center gap-4">
+        {token && userData ? (
+          <div className="flex items-center gap-2 cursor-pointer relative group">
+            <img
+              className="w-8 rounded-full"
+              src={userData.image}
+              alt="profile_pic"
+            />
+            <img
+              className="w-2.5"
+              src={assets.dropdown_icon}
+              alt="dropdown_icon"
+            />
 
-                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden'>
-                    <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                        <p onClick={()=> navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                        <p onClick={()=> navigate("/my-appointments")} className='hover:text-black cursor-pointer'>My Appointments</p>
-                        <p onClick={logout} className='hover:text-black cursor-pointer'>Logout</p>
-                    </div>
-                </div>
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p
+                  onClick={() => navigate("/my-profile")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Profile
+                </p>
+                <p
+                  onClick={() => navigate("/my-appointments")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Appointments
+                </p>
+                <p onClick={logout} className="hover:text-black cursor-pointer">
+                  Logout
+                </p>
+              </div>
             </div>
-            : <button
-                onClick={() => navigate('/login')}
-                className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer'>
-                    Create account
-            </button>
-        }
-        <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt='menu_icon'/>
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer"
+          >
+            Create account
+          </button>
+        )}
+        <img
+          onClick={() => setShowMenu(true)}
+          className="w-6 md:hidden"
+          src={assets.menu_icon}
+          alt="menu_icon"
+        />
         {/*------- MOBILE MENU -------*/}
-        <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
-            <div className='flex items-center justify-between px-5 py-6'>
-                <img className='w-36' src={assets.logo} alt="logo" />
-                <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="cross_icon" />
-            </div>
-            <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
-                <NavLink className='px-4 py-2 rounded inline-block' onClick={() => setShowMenu(false)} to='/'>HOME</NavLink>
-                <NavLink className='px-4 py-2 rounded inline-block' onClick={() => setShowMenu(false)} to='/doctors'>ALL DOCTORS</NavLink>
-                <NavLink className='px-4 py-2 rounded inline-block' onClick={() => setShowMenu(false)} to='/about'>ABOUT</NavLink>
-                <NavLink className='px-4 py-2 rounded inline-block' onClick={() => setShowMenu(false)} to='/contact'>CONTACT</NavLink>
-            </ul>
+        <div
+          className={`${showMenu ? "fixed w-full" : "h-0 w-0"} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+          <div className="flex items-center justify-between px-5 py-6">
+            <img className="w-36" src={assets.logo} alt="logo" />
+            <img
+              className="w-7"
+              onClick={() => setShowMenu(false)}
+              src={assets.cross_icon}
+              alt="cross_icon"
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink
+              className="px-4 py-2 rounded inline-block"
+              onClick={() => setShowMenu(false)}
+              to="/"
+            >
+              HOME
+            </NavLink>
+            <NavLink
+              className="px-4 py-2 rounded inline-block"
+              onClick={() => setShowMenu(false)}
+              to="/doctors"
+            >
+              ALL DOCTORS
+            </NavLink>
+            <NavLink
+              className="px-4 py-2 rounded inline-block"
+              onClick={() => setShowMenu(false)}
+              to="/about"
+            >
+              ABOUT
+            </NavLink>
+            <NavLink
+              className="px-4 py-2 rounded inline-block"
+              onClick={() => setShowMenu(false)}
+              to="/contact"
+            >
+              CONTACT
+            </NavLink>
+
+            {token && (
+              <>
+                <hr className="w-full my-2" />
+
+                <NavLink
+                  className="px-4 py-2 rounded inline-block"
+                  onClick={() => setShowMenu(false)}
+                  to="/my-profile"
+                >
+                  MY PROFILE
+                </NavLink>
+
+                <NavLink
+                  className="px-4 py-2 rounded inline-block"
+                  onClick={() => setShowMenu(false)}
+                  to="/my-appointments"
+                >
+                  MY APPOINTMENTS
+                </NavLink>
+
+                <button
+                  onClick={() => {
+                    logout();
+                    setShowMenu(false);
+                  }}
+                  className="px-4 py-2 text-red-500"
+                >
+                  LOGOUT
+                </button>
+              </>
+            )}
+          </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
