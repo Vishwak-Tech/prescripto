5e9cf0d5ac3cd01627fe7f5ec9c226fa90063a43
Vishwak Koleti <koletivishwak84@gmail.com>
2026-06-13 20:55:39 +0530

    UI Updates Done

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -26,15 +26,31 @@ const Navbar = () => {
     navigate('/')
 }
 
-  return (
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
-      <div className='flex items-center gap-2 text-xs'>
-        <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="admin_logo" />
-        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
-      </div>
-      <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full cursor-pointer'>Logout</button>
+return (
+  <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+    <div className='flex items-center gap-2 text-xs'>
+      <a href="https://prescripto-frontend-tau-five.vercel.app">
+        <img
+          className='w-36 sm:w-40 cursor-pointer'
+          src={assets.admin_logo}
+          alt="admin_logo"
+        />
+      </a>
+
+      <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>
+        {aToken ? 'Admin' : 'Doctor'}
+      </p>
     </div>
-  )
+
+    <button
+      onClick={logout}
+      className='bg-primary text-white text-sm px-10 py-2 rounded-full cursor-pointer'
+    >
+      Logout
+    </button>
+  </div>
+)
+  
 }
 
 export default Navbar
