3b0e6bfb61cadc72e4fc11fbd3624ad4c4d2b63d
Vishwak Koleti <koletivishwak84@gmail.com>
2026-09-20 13:22:36 +0530

    fix: configure razorpay and production navigation

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 return (
   <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
     <div className='flex items-center gap-2 text-xs'>
-      <a href="https://prescripto-frontend-tau-five.vercel.app">
+      <a href="https://prescripto.shop/">
         <img
           className='w-36 sm:w-40 cursor-pointer'
           src={assets.admin_logo}
